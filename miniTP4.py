ff180c7a19a9a5aa7ade23995ae2582a00aea0d1
Sylvain Madoumier <sylvain.madoumier87@gmail.com>
2022-12-05 15:23:25 +0100

    5

--- miniTP4.py
+++ miniTP4.py
@@ -2,8 +2,25 @@
 #     f.write("Hello World\n")
 
 
+# import sys
+
+# fichier, content, fin, nombre = sys.argv[1], sys.argv[2], sys.argv[3],sys.argv[4]
+# nb = int(nombre)
+# with open(fichier, "a") as f:
+#     while nb < 0 :
+#         f.write(content)
+#         nb-=1
+    
+#     f.write(fin)
+    
 import sys
 
-fichier, content = sys.argv[1], sys.argv[2]
+fichier, content, fin, nombre = sys.argv[1], sys.argv[2], sys.argv[3],sys.argv[4]
+
+nb = int(nombre)
 with open(fichier, "a") as f:
-    f.write(content)
+    while nb > 0 :
+        f.write(content)
+        nb-=1
+
+    f.write(fin)
