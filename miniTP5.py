b2f6ba92749118553848c179076edd61f2d5cd89
Sylvain Madoumier <sylvain.madoumier87@gmail.com>
2022-12-05 16:07:50 +0100

    7

--- miniTP5.py
+++ miniTP5.py
@@ -1,19 +1,28 @@
 import sys
+# import os
 
 fichier, content, fin= sys.argv[1], sys.argv[2], sys.argv[3]
-if sys.argv[4] is not None:
+# print(len(sys.argv))
+if len(sys.argv) >=5:
     nombre = sys.argv[4]
     nb = int(nombre)
 
 try:
     with open(fichier, "a") as f:
-        if nb is not None:
+        if len(sys.argv) >=5:
             while nb > 0 :
-                f.write(content,"\n")
+                f.write(f"{content}\n")
                 nb-=1
         else:
-            f.write(content)
+            f.write(f"{content}\n")
             
         f.write(fin)
 except:
-    print("error")
+    print("error write")
+
+
+# try:
+#     with open(fichier, "a") as f:
+#         print(fichier)
+# except:
+#     print("error read")
