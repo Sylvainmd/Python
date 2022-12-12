0fbd662063dd9d5f4f2dd396b0cca93222854a2b
Sylvain Madoumier <sylvain.madoumier87@gmail.com>
2022-12-12 16:46:47 +0100

    1

--- ssh.py
+++ ssh.py
@@ -6,7 +6,7 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect('192.168.1.16', username='root', password="debian")
 
-stdin, stdout, stderr = ssh.exec_command('ls -l')
+stdin, stdout, stderr = ssh.exec_command('ls -l') #ont met la commande que l'ont souhaite ici
 
 print(stdout.readlines())
 
