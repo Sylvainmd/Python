7376ac70fbf32c6010da6f327abe98084b69ea0b
Sylvain Madoumier <sylvain.madoumier87@gmail.com>
2023-01-09 10:21:43 +0100

    .

--- ssh3 compte ad.py
+++ ssh3 compte ad.py
@@ -1,15 +1,16 @@
 import paramiko
 
-host = "192.168.1.16"
-port = 22
-transport = paramiko.Transport((host, port))
+ssh = paramiko.SSHClient()
 
-password = "debian"
-username = "root"
-transport.connect(username = username, password = password)
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('192.168.1.16', username='Sylvain', password="Sylvian$C87")
 
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-source_folder = "C:\Users\sylva\OneDrive\Bureau\Scripting systeme\Python\miniTP1.py"
+source_folder = "C:\Users\sylva\OneDrive\Bureau\Scripting systeme\Python\creacompte.txt"
 inbound_files = sftp.listdir(source_folder)
 
+ssh.close()
+
+print("Création utilisateur fini")
